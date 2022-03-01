185815af51e93b45976bf8a448be9a98cb91a501
jensone54 <42637127+jensone54@users.noreply.github.com>
2022-03-01 08:38:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
+export REPOSITORY= https://github.com/jensone54/acr-build-helloworld-node 
 if [[ ! -v REPOSITORY ]]; then
     echo "REPOSITORY env variable not set, should be in the format of a URL e.g. https://github.com/energinet-datahub/eo-selfhosted-runner"
     exit 1;
 fi
-
+export TOKEN=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
 if [[ ! -v TOKEN ]]; then
     echo "TOKEN env variable not set, is generated on the GitHub API or WebSite."
     exit 1;
 fi
-
+export NAME=EphemeralRunner
 if [[ ! -v NAME ]]; then
     echo "NAME env variable not set, replaces existing runner with same name, since the container is ephemeral."
     exit 1;
